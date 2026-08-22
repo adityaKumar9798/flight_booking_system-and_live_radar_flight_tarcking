d29560831a349029822282a3285631ec8b54a870
adityaKumar9798 <adkmr2255@gmail.com>
2026-08-22 21:39:04 +0530

    chore: move firebase config to env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCisXoHVcV7wXgSRirG8UpznZvuOU7hsbU",
-  authDomain: "flight911-885f1.firebaseapp.com",
-  projectId: "flight911-885f1",
-  storageBucket: "flight911-885f1.firebasestorage.app",
-  messagingSenderId: "1058005031809",
-  appId: "1:1058005031809:web:c45646d3082ca1efd1e680"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase (prevent multiple initializations during hot reload in Next.js)
